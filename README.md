022ff8dd01f22e2ff7a42b133cdeaf88a1076983
Tim Caswell <tim@creationix.com>
2024-06-30 15:55:18 -0500

    Add some stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # musical-learning
+
+client id: 17e75ea0bd3b4c5ba42390857ac12dd4
+
+Set client secret in `CLIENT_SECRET`.
---
+++ app.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('p')
+    .forEach(p =>
+        p.onclick = () =>
+            p.style.color = randomColor()
+    )
+
+function randomColor() {
+    return `hsl(${Math.random() * 360}, 100%, 50%)`
+}
+const clientId = '17e75ea0bd3b4c5ba42390857ac12dd4'
+const clientSecret = localStorage.getItem('client_secret')
+console.log({ clientId, clientSecret })
