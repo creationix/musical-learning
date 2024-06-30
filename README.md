f58f8160021b0a84b8997c76c820ab0068910cfe
Tim Caswell <tim@creationix.com>
2024-06-30 16:07:42 -0500

    Save Progress

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 client id: 17e75ea0bd3b4c5ba42390857ac12dd4
 
-Set client secret in `CLIENT_SECRET`.
+Set client secret manually local storage `client_secret`.
+
+```js
+localStorage.setItem('client_secret', ...)
+```
--- app.js
+++ app.js
@@ -7,6 +7,8 @@ document.querySelectorAll('p')
 function randomColor() {
     return `hsl(${Math.random() * 360}, 100%, 50%)`
 }
+
 const clientId = '17e75ea0bd3b4c5ba42390857ac12dd4'
 const clientSecret = localStorage.getItem('client_secret')
 console.log({ clientId, clientSecret })
+
